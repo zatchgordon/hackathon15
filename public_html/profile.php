31e40e0906fdb3d1ef5d615102e5113de03cdbd9
zatch <zacgord@gmail.com>
2015-11-20 22:54:20 -0500

    changed some profile.php stuff

--- public_html/profile.php
+++ public_html/profile.php
@@ -5,13 +5,27 @@
 	
 	$id = $_GET['id'];
 
-	$stmt = $db->prepare("SELECT DISTINCT u.fname, u.lname, s.sname, m.mname, e.ename, d.dname, j.jname FROM User u JOIN CC_AS ccas ON ccas.uid = u.uid JOIN S_AS sas ON sas.uid = u.uid JOIN E_AS eas ON eas.uid = u.uid JOIN Career_Cluster cc ON cc.ccid = ccas.ccid JOIN School s ON s.sid = sas.sid JOIN Degree d ON d.did = sas.did JOIN Major m ON m.mid = sas.mid JOIN Employer e ON e.eid = eas.eid JOIN Job j ON j.jid = eas.jid WHERE u.uid = '$id'");
+	$stmt = $db->prepare("SELECT DISTINCT * FROM User u JOIN CC_AS ccas ON ccas.uid = u.uid JOIN S_AS sas ON sas.uid = u.uid JOIN E_AS eas ON eas.uid = u.uid JOIN Career_Cluster cc ON cc.ccid = ccas.ccid JOIN School s ON s.sid = sas.sid JOIN Degree d ON d.did = sas.did JOIN Major m ON m.mid = sas.mid JOIN Employer e ON e.eid = eas.eid JOIN Job j ON j.jid = eas.jid WHERE u.uid = '$id'");
 	$stmt->execute();
 
+	$first = true;
 
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		if ($first){
 			echo $row['lname'].', '. $row['fname'];
 			echo "<br>";
+			echo $row['dname'].' '. $row['mname'];
+			$about = $row['about'];
+			$first = false;
+			echo "<br>";
+		}
+			
+		if ($first){
+			echo $row['about'].'<br>';
+		}
+			echo $row['ename'].'<br>';
+			echo $row['start'] . ' - ' . $row['end'] . '<br>';
+
 	}
 
 
