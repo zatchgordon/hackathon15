05248876899a719dbab84c31dc1b551549463223
zatch <zacgord@gmail.com>
2015-11-21 00:06:52 -0500

    not much has changed

--- public_html/profile.php
+++ public_html/profile.php
@@ -5,7 +5,7 @@
 	
 	$id = $_GET['id'];
 
-	$stmt = $db->prepare("SELECT DISTINCT * FROM User u JOIN CC_AS ccas ON ccas.uid = u.uid JOIN S_AS sas ON sas.uid = u.uid JOIN E_AS eas ON eas.uid = u.uid JOIN Career_Cluster cc ON cc.ccid = ccas.ccid JOIN School s ON s.sid = sas.sid JOIN Degree d ON d.did = sas.did JOIN Major m ON m.mid = sas.mid JOIN Employer e ON e.eid = eas.eid JOIN Job j ON j.jid = eas.jid WHERE u.uid = '$id'");
+	$stmt = $db->prepare("SELECT DISTINCT * FROM User u JOIN CC_AS ccas ON ccas.uid = u.uid JOIN S_AS sas ON sas.uid = u.uid JOIN E_AS eas ON eas.uid = u.uid JOIN Career_Cluster cc ON cc.ccid = ccas.ccid JOIN School s ON s.sid = sas.sid JOIN Degree d ON d.did = sas.did JOIN Major m ON m.mid = sas.mid JOIN Employer e ON e.eid = eas.eid JOIN Job j ON j.jid = eas.jid WHERE u.uid = '$id' ORDER BY start DESC");
 	$stmt->execute();
 
 	$first = true;
@@ -16,15 +16,19 @@
 			echo "<br>";
 			echo $row['dname'].' '. $row['mname'];
 			$about = $row['about'];
-			$first = false;
 			echo "<br>";
 		}
 			
 		if ($first){
-			echo $row['about'].'<br>';
+			echo "ABOUT: <br>";
+			echo $row['about'].'<br><br>';
+			$first = false;
+
+			echo "EXPERIENCE: <br>";
 		}
 			echo $row['ename'].'<br>';
 			echo $row['start'] . ' - ' . $row['end'] . '<br>';
+			echo $row['jname'].'<br> <br>';
 
 	}
 
